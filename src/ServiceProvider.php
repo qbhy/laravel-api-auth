902409ada157c167436d886eecbc8a86b9d8efb6
96qbhy <96qbhy@gmail.com>
2017-11-20 11:21:12 +0800

    优化开发体验

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -19,7 +19,7 @@ class ServiceProvider extends LaravelServiceProvider
     protected function setupConfig()
     {
         $configSource = realpath(__DIR__ . '/config.php');
-        $middlewareSource = realpath(__DIR__ . '/middleware.php');
+        $middlewareSource = realpath(__DIR__ . '/middleware');
         if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
             $this->publishes([
                 $middlewareSource => app_path('Http/Middleware/LaravelApiAuth.php'),
